399d712c569982f3b5e16e5e8feb2b813d0238c1
Drew Mitchell <hi@callmedrew.com>
2015-07-26 01:06:42 -0400

    Adding /setup/ folder to help set the website up in other environments.

---
+++ setup/config.php
@@ -0,0 +1,11 @@
+<?php
+$sql_host = '';
+$sql_username = '';
+$sql_password = '';
+$sql_database = '';
+
+// These directories should contain sprites named by PokéDex ID
+// without leading zeros (e.g. "25.gif").
+$path_to_sprites = '/sprites/animated/';
+$path_to_shiny_sprites = '/sprites/animated/shiny/';
+$sprite_extention = '.gif';
