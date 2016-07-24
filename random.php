7cdf00b3a39779fddf43ed10f420f0e020175e68
Drew Mitchell <hi@callmedrew.com>
2016-07-24 00:55:20 -0400

    More efficient querying of tiers.

--- random.php
+++ random.php
@@ -1,4 +1,4 @@
-<?php 
+<?php
 // This page reads in $_GET URL parameters, validates them, generates random Pokémon
 // based on the parameters, and outputs the result in JSON.
 
@@ -35,8 +35,32 @@ $type = get_or_set("type", null, $type_list);
 $paramArray = array();
 if ($region != null) {$paramArray[] = $region . " = 1";}
 if ($type != null) {$paramArray[] = '(type1 = "' . $type . '" OR type2 = "' . $type . '")';}
-if ($ubers == false) {$paramArray[] = 'tier != "Uber"';}
-if ($nfes == false) {$paramArray[] = 'tier != "NFE"';}
+if ($ubers && $nfes) {
+	// If we want to get ubers and NFEs as well as fully evolved Pokemon,
+	// no need to add a parameter for that.
+} else if ($ubers == false && $nfes == false) {
+	// No Ubers and no NFEs - only fully evolved Pokemon.
+	$paramArray[] = 'tier = "FE"';
+} else {
+	// We want to query for 2 of the 3 tiers, leaving out either Ubers or NFEs.
+	if (count($paramArray) == 0) {
+		// If there are no parameters so far, it's more efficient to take the
+		// union of two equalities rather than one inequality.
+		if ($ubers) {
+			$paramArray[] = '(tier = "FE" OR tier = "Uber")';
+		} else if ($nfes) {
+			$paramArray[] = '(tier = "FE" OR tier = "NFE")';
+		}
+	} else {
+		// If there already are some other parameters, it's more efficient to use
+		// the index for them remove the unwanted tier by inequality.
+		if ($ubers) {
+			$paramArray[] = '(tier != "NFE")';
+		} else if ($nfes) {
+			$paramArray[] = '(tier != "Uber")';
+		}
+	}
+}
 $parameters = (count($paramArray) > 0) ? "WHERE " . implode(" AND ", $paramArray) : "";
 
 // Connect to the database and execute the query.
@@ -53,7 +77,7 @@ while($row = $dbOutput->fetch_assoc()) {
 	if ($natures) {
 		$row['nature'] = $nature_list[mt_rand(0, count($nature_list)-1)];
 	}
-	
+
 	$outputArray[] = $row;
 }
 
