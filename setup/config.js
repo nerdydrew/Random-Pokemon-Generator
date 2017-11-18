52032fed17c3c4414d543e3efe08dcb9c3ab92b4
Drew Mitchell <hi@callmedrew.com>
2017-11-17 21:43:16 -0500

    Update config.js

--- setup/config.js
+++ setup/config.js
@@ -9,6 +9,6 @@ function logToAnalytics(url) {
 
 // These directories should contain sprites named by PokéDex ID
 // without leading zeros (e.g. "25.gif").
-const PATH_TO_ANIMATED_SPRITES = 'sprites/animated/';
-const PATH_TO_SHINY_ANIMATED_SPRITES = 'sprites/animated/shiny/';
-const ANIMATED_SPRITE_EXTENTION = '.gif';
+const PATH_TO_SPRITES = 'sprites/animated/';
+const PATH_TO_SHINY_SPRITES = 'sprites/animated/shiny/';
+const SPRITE_EXTENTION = '.gif';
